3f2527c46e331ce2e65e1cc6b5f0829a2aebc8b1
Arunkumar S <104545860+arunkumaa6@users.noreply.github.com>
2022-07-11 10:23:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+        <div className='link'>
+    <a href='' className='a1'>Home</a>
+    <a href='' className='a2'>logo</a>
+    <a href='' className='a3'>cart</a>
+    <a href='' className='a4'>P</a>
+    </div>
+    </div>
+  )
+}
+
+export default App
